72ccaacc3c30afe6eb1be125d500df6f3c03cbb7
Ryan Miller <ryan@devopsmachine.com>
2018-06-28 15:13:30 -0400

    Update profile.ps1

--- Windows/profile.ps1
+++ Windows/profile.ps1
@@ -1,4 +1,8 @@
+$POWERSHELL_PROFILE = "${env:USERPROFILE}\Documents\WindowsPowerShell\profile.ps1"
+
+#
 # Create an empty file
+#
 function Touch-File {
 	$file = $args[0]
 	if($file -eq $null) {
@@ -7,16 +11,19 @@ function Touch-File {
 	if(Test-Path $file) {
 		throw "File already exists"
 	} else {
-		$null > $file
+		"" > $file
 	}
 }
 Set-Alias -Name touch -Value Touch-File
 
+
+#
 # Open notepad++ (optionally pass file to open)
+#
 function Open-Notepad-Plus-Plus {
 	$file = $args[0]
 	$program = "C:\Program Files (x86)\Notepad++\notepad++.exe"
-	
+
 	if($file -eq $null) {
 		& $program
 	} else {
@@ -26,9 +33,32 @@ function Open-Notepad-Plus-Plus {
 		& $program $file
 	}
 }
-Set-Alias -Name npp -Value Open-Notepad-Plus-Plus
+Set-Alias -Name edit -Value Open-Notepad-Plus-Plus
 
+
+#
+# Sign a PowerShell script with signing certificate
+#
+function Sign-PowerShell-Script {
+	$cert = $(Get-ChildItem Cert:\CurrentUser\My\ -CodeSigningCert)
+	Set-AuthenticodeSignature ${args}[0] -Certificate ${cert} > $null
+}
+Set-Alias -Name sign -Value Sign-PowerShell-Script
+
+
+#
+# Sign and then run a PowerShell script
+#
+function Sign-And-Run-PowerShell-Script {
+	sign ${args}[0]
+	Invoke-Expression -Command ${args}[0]
+}
+Set-Alias -Name run -Value Sign-And-Run-PowerShell-Script
+
+
+#
 # Customize command prompt to be on two lines
+#
 function prompt {
 	If (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -36,10 +66,15 @@ function prompt {
 		$u = "ADMIN "
 	}
 	$p = $pwd."Path"
-	
+
+	Write-Host ""
 	Write-Host "PS " -nonewline
 	Write-Host "${u}" -nonewline -ForegroundColor Red
 	Write-Host "${p}`n>" -nonewline
-	
+
 	return " "
 }
+
+
+# Make things neat
+Clear-Host
